25078c8f42e248cdf1254034b523718ae1e973af
LoicBrassart <loic.brassart@gmail.com>
2021-10-29 18:54:57 +0200

    Add support for several pages + Create mock homepage & scenarii page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Switch, Route } from 'react-router-dom';
 import Layout from 'Layout';
+import { HomePage, ScenariiPage } from 'Pages';
 
 function App() {
   return (
     <Layout>
-      <main>Main</main>
+      <main>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/scenarii" component={ScenariiPage} />
+        </Switch>
+      </main>
     </Layout>
   );
 }
