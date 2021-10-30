5e9d7193a7b29d211ef958a32ead343bd4ab57a4
LoicBrassart <loic.brassart@gmail.com>
2021-10-30 18:10:41 +0200

    Update mockData of every type to have one specific field to display in search results

---
+++ src/mockData/index.js
@@ -0,0 +1,17 @@
+import mockBlog from './blog.json';
+import mockCharacters from './characters.json';
+import mockGames from './games.json';
+import mockNews from './news.json';
+import mockParties from './parties.json';
+import mockScenarii from './scenarii.json';
+import mockUsers from './users.json';
+
+export {
+  mockBlog,
+  mockCharacters,
+  mockGames,
+  mockNews,
+  mockParties,
+  mockScenarii,
+  mockUsers,
+};
