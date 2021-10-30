48cf470206f4e78860538e75aedddefe4ccd7b00
LoicBrassart <loic.brassart@gmail.com>
2021-10-31 01:12:50 +0200

    Add component to abstract data.map and provide a better DX

---
+++ src/Ui/List/style.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const SList = styled.div`
+  h3 {
+    text-transform: uppercase;
+    border-bottom: 2px solid white;
+  }
+
+  ul {
+    display: flex;
+    flex-flow: row wrap;
+  }
+`;
+
+export default SList;
---
+++ src/Ui/index.js
@@ -0,0 +1,5 @@
+import List from './List';
+
+const toto = 42;
+
+export { List, toto };
